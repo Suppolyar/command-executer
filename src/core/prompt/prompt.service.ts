e763fa1358416ffc6490c21fccb42d833104ce49
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2023-09-17 21:54:21 +0400

    feat (core): Create a prompt service

---
+++ src/core/prompt/prompt.service.ts
@@ -0,0 +1,14 @@
+import inquirer from 'inquirer';
+import type { PromptType } from "./prompt.types";
+export class PromptService {
+    public async input<T>(message: string, type: PromptType) {
+        const { result } = await inquirer.prompt<{ result: T }>([
+            {
+                type: type,
+                name: 'result',
+                message
+            }
+        ])
+        return result
+    }
+}
